9140682080357d208ab29b21cc07a6de11fedfcf
kei <kei-s@mbc.nifty.com>
2011-04-29 23:39:54 +0900

    add Sessions#create, destroy, failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  def create
+    auth = env['omniauth.auth']
+    unless user = User.find_by_provider_and_uid(auth['provider'], auth['uid'])
+      user = User.new({
+        :name => auth['user_info']['nickname'],
+        :provider => auth['provider'],
+        :uid => auth['uid'],
+        :access_token => auth['credentials']['token'],
+        :access_secret => auth['credentials']['secret'],
+      })
+      user.save!
+    end
+    self.current_user = user
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    self.sign_out!
+    redirect_to root_path
+  end
+
+  def failure
+    logger.warn "Auth Failed: " + params[:message]
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Intr::Application.routes.draw do
   root :to => 'welcome#index'
   get '/dashboard', :to => 'welcome#dashboard'
+
+  delete 'sessions/destroy'
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
 end
